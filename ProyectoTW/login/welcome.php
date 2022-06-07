42c6d167bb6b77b4d56054981be212e249ffce17
JairVqz <jair10.vasquez11@gmail.com>
2022-06-07 10:54:36 -0500

    menú responsive

--- ProyectoTW/login/welcome.php
+++ ProyectoTW/login/welcome.php
@@ -1,31 +1,30 @@
 <?php
 // Initialize the session
 session_start();
- 
+
 // Check if the user is logged in, if not then redirect him to login page
-if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
     header("location: login.php");
     exit;
 }
 ?>
- 
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Bienvenido Administrador</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="css/estilos.css">
-    <style>
-        body{ font: 14px sans-serif; text-align: center; }
-    </style>
 </head>
+
 <body>
 
-<div class="usuario">
+    <div class="usuario">
         <?php
         $usuario = $_SESSION['username'];
-        echo "<p><h3>$usuario</h3></p>";
+        echo "<p><h4>$usuario</h4></p>";
         ?>
         <figure>
             <button type="button" onclick="document.location='logout.php'"> <img src="img/logout.png" height="50px" width="50px"> </button>
@@ -34,26 +33,52 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     </div>
 
     <div class="container" role="main">
-        
-        <div class="c1">
 
+        <div class="c1">
             <div class="menu">
-                <a href="#">Acerca de nosotros</a>
-                <a href="#">Registro de solicitud</a>
-                <a href="../admin/listaUsuarios/alumno.php">Lista de usuarios</a>
-                <a href="../admin/roles/roles.php">Roles</a>
-                <a href="#">Bitácora de uso</a>
-                <a href="#">Observaciones</a>
-                <a href="../admin/equipos/equipos.php">Equipos</a>
-                <a href="#">Chat</a>
+                <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                    <div class="container-fluid">
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarNav">
+                            <ul class="navbar-nav">
+                            <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="../login/welcome.php">Acerca de Nosotros</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="#">Registro de solicitud</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="../admin/listaUsuarios/alumno.php">Lista de usuarios</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="../admin/roles/roles.php">Roles</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Bitácora de uso</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Observaciones</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="../admin/equipos/equipos.php">Equipos</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="../chat/paginaChat">Chat</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
             </div>
 
             <div class="contenido">
                 <hr>
                 <h1>Bienvenido</h1>
-                <p>El Centro de Cómputo de la Facultad de Estadística e Informática es una 
-                    unidad, la cual ofrece a la comunidad de la Universidad Veracruzana 
-                    la posibilidad de utilizar y pedir prestado el equipo computacional cuando sea requerido. 
+                <p>El Centro de Cómputo de la Facultad de Estadística e Informática es una
+                    unidad, la cual ofrece a la comunidad de la Universidad Veracruzana
+                    la posibilidad de utilizar y pedir prestado el equipo computacional cuando sea requerido.
                 </p>
                 <p>Este sistema fue elaborado con el objetivo de facilitar diversas tareas al personal del Centro de Cómputo de la Facultad
                     de Estadística e Informática, buscando automatizar y sistematizar algunas tareas, como lo son en este caso
@@ -64,13 +89,16 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
         </div>
 
         <!--<h1 class="my-5">Hola, <b><!?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Bienvenido al Sitio de Administador.</h1>-->
-    <p>
-        <a href="reset-password.php" class="btn btn-warning">¿Olvidaste tu contraseña?</a>
-        <a href="logout.php" class="btn btn-danger ml-3">Cerrar Sesión</a>
-    </p>
+        <p>
+            <a href="reset-password.php" class="btn btn-warning">¿Olvidaste tu contraseña?</a>
+            <a href="logout.php" class="btn btn-danger ml-3">Cerrar Sesión</a>
+        </p>
 
     </div>
 
-    
+
 </body>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
 </html>
--- ProyectoTW/login/welcome2.php
+++ ProyectoTW/login/welcome2.php
@@ -1,29 +1,87 @@
 <?php
 // Initialize the session
 session_start();
- 
+
 // Check if the user is logged in, if not then redirect him to login page
-if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
     header("location: login.php");
     exit;
 }
 ?>
- 
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Bienvenido Alumno</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <style>
-        body{ font: 14px sans-serif; text-align: center; }
-    </style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/estilos.css">
 </head>
+
 <body>
-    <h1 class="my-5">Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Bienvenido al Sitio de Alumno.</h1>
+
+    <div class="usuario">
+        <?php
+        $usuario = $_SESSION['username'];
+        echo "<p><h4>$usuario</h4></p>";
+        ?>
+        <figure>
+            <button type="button" onclick="document.location='logout.php'"> <img src="img/logout.png" height="50px" width="50px"> </button>
+            <figcaption>Cerrar Sesión</figcaption>
+        </figure>
+    </div>
+
+    <div class="container" role="main">
+
+        <div class="c1">
+            <div class="menu">
+                <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                    <div class="container-fluid">
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarNav">
+                            <ul class="navbar-nav">
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="../login/welcome2.php">Acerca de Nosotros</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="#">Registro de solicitud</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Observaciones</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="../chat/paginaChat">Chat</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+
+            <!--div class="contenido">
+                <hr>
+                <h1>Bienvenido</h1>
+                <p>El Centro de Cómputo de la Facultad de Estadística e Informática es una
+                    unidad, la cual ofrece a la comunidad de la Universidad Veracruzana
+                    la posibilidad de utilizar y pedir prestado el equipo computacional cuando sea requerido.
+                </p>
+                <p>Este sistema fue elaborado con el objetivo de facilitar diversas tareas al personal del Centro de Cómputo de la Facultad
+                    de Estadística e Informática, buscando automatizar y sistematizar algunas tareas, como lo son en este caso
+                    el registro de solicitudes de prétamos del equipo de cómputo.
+                </p>
+            </div-->
+
+        </div>
+    </div>
+
+    <!--h1 class="my-5">Hi, <b><!?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Bienvenido al Sitio de Alumno.</h1-->
     <p>
         <a href="reset-password.php" class="btn btn-warning">¿Olvidaste tu contraseña?</a>
         <a href="logout.php" class="btn btn-danger ml-3">Cerrar Sesión</a>
     </p>
 </body>
+
 </html>
